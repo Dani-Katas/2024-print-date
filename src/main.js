c80752f2e9c479a14c83d3ac2f60a1e75a226b5f
Daniel Ramos <danielramosacosta1@gmail.com>
2024-06-15 13:18:07 +0200

    programa terminado

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
-export function myFunction() {
-  return true
+import * as fs from "fs"
+
+const [binPath, filePath, name, age] = process.argv
+
+if (!name || !age) {
+  const users = JSON.parse(fs.readFileSync("./users.json", "utf8"))
+
+  console.log(users)
+  process.exit(0)
 }
+
+const user = {
+  name,
+  age: parseInt(age),
+  createdAt: new Date(),
+}
+
+const users = JSON.parse(fs.readFileSync("./users.json", "utf8"))
+
+const alreadyExists = users.some((savedUser) => savedUser.name === name)
+
+if (alreadyExists) {
+  throw new Error("User already exists")
+}
+
+users.push(user)
+
+fs.writeFileSync("./users.json", JSON.stringify(users, null, 2))
